117dc50cedc7854535fee85643319247045afeaf
yuchen <353060009@qq.com>
2020-04-11 21:38:13 +0800

    把本地工程添加到mit

---
+++ git-test-project/src/com/itheima/git/firstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class firstClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
